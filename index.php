91beb4404124e00b9eac94a1896e055765455f27
show <showshou@126.com>
2015-06-29 20:59:46 +0800

    chu

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+define("ANYWAY",dirname(__FILE__));
+header("Content-type: text/html; charset=utf-8"); 
+include ANYWAY."/show/core.php";
+if(!defined('ANYWAYX')){exit();}
+?>
---
+++ show/core.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * 核心加载
+ * Author show 
+ * copry right PHPBODY (www.anyway.cn)
+ */
+//error_reporting(E_ALL);
+define("ANYWAYX","show");
+
+?>
